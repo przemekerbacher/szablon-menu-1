17e87615351209d51f8fac60958a51597c31cd1a
Przemek <przemyslaw.erbaher@gmail.com>
2020-04-22 14:17:41 +0200

    mobile customization, add basket

--- js/site.js
+++ js/site.js
@@ -13,3 +13,53 @@ if (addToBasketButtons) {
     });
   });
 }
+
+//display-category
+var categorySelectors = document.querySelectorAll(".category-selector");
+if (categorySelectors)
+  categorySelectors.forEach((categorySelector) => {
+    categorySelector.addEventListener("click", function () {
+      const categories = document.querySelectorAll(".category");
+      const target = document.querySelector(this.getAttribute("data-target"));
+      moveActiveClass(categorySelector, categorySelectors);
+      moveActiveClass(target, categories);
+    });
+  });
+
+moveActiveClass = (selected, all) => {
+  all.forEach((element) => {
+    if (element === selected) {
+      element.classList.add("active");
+    } else {
+      element.classList.remove("active");
+    }
+  });
+};
+
+//add collapsible effect
+const collasibleElements = document.querySelectorAll(
+  ".collapsible, .button-collapsible"
+);
+
+if (collasibleElements)
+  collasibleElements.forEach((element) => {
+    element.addEventListener("click", function (e) {
+      const content = document.querySelector(
+        e.currentTarget.getAttribute("data-target")
+      );
+
+      e.currentTarget.classList.toggle("active");
+      toggleDisplay(content);
+    });
+  });
+
+const toggleDisplay = (content) => {
+  if (content)
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+      content.classList.remove("active");
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+      content.classList.add("active");
+    }
+};
